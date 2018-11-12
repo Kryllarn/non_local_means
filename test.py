0d18c6d8bd1ab59c222fd8102221fbbcaf4fa608
Shurizai <antoinedu02300@hotmail.fr>
2018-11-12 15:15:53 +0100

    Test de parallel, efficacité assez limitée, passage de 10s à 6s en ayant esperé mieux

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
-Hello my friend
+from multiprocessing import Pool
+
+def groscalcul(n):
+    k = 0
+    for i in range(n):
+        k += i
+    return k
+
+
+pool = Pool()
+
+nb = 30000000
+
+args = [nb, nb + 10, nb*2]
+
+res = pool.map(groscalcul, args)
+
+for i in res:
+    j = 1
+    print("res",j, ":", i)
+    j += 1

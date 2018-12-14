0aa5f3cfb378d453e40d468bd750999a5e7afb94
Kryllarn <kryllarn@gmail.com>
2018-12-14 18:00:38 +0100

    Edit Denoiser class

--- Denoiser.py
+++ Denoiser.py
@@ -1,70 +1,108 @@
 #!/usr/bin/env python
 # -*- coding:Utf-8 -*-
 
-import Patch
-from PIL import Image as img
-import Window
+from Patch import *
+from PIL import Image
 
 
-def init_tab_patch(image, sizePatch):
-    tab_patch = [[]]
-    for x in range(image.width()):
-        for y in range(image.height()):
-            tab_patch[x][y] = Patch((x, y), sizePatch, image)
+class Denoiser:
 
-    return tab_patch
+    def __init__(self, filename):
+        self.noised_image = Image.open(filename)
+        self.prepare_image()
+        self.denoised_image = Image.new("""L""", (self.noised_image.size[0], self.noised_image.size[1]))
+        self.denoised_image.save("""pictures/output.png""")
+        self.patchs_array = [[]]
+        self.closest_patchs_array = [[]]
+        self.closest_patchs_array_current_size = 0
+        self.closest_patchs_array_maximum_size = 5
+    # end def
 
+    @staticmethod
+    def display_image_details(image):
+        print("""{ Format :""", image.format,
+              """; Mode :""", image.mode,
+              """; Size :""", image.size, """}""")
+    # end def
 
-def dist_max(tab_patch):
-    ind = 0
-    max = tab_patch[0][2]
-    for i in range(tab_patch.__sizeof__):
-        if max < tab_patch[i][2]:
-            ind = i
-    return ind
-
-
-def run_denoiser(image_name, sizePatch, sizeWindow):
-    image = img.open(image_name)
-    tab_patch = init_tab_patch(image, sizePatch)
-    closest_patch = []
-    NB_CLOSEST = 5  # definition du nombre de patchs minimum
-    res_image = img.new('L', image.size(), color=0)
-    # parcours de l'image
-    for x in range(image.width()):
-        for y in range(image.height()):
-            # parcours de la fenetre
-            for u in range(sizeWindow):
-                for t in range(sizeWindow):
-                    if (x + u - ((sizeWindow - 1) / 2) >= 0 and
-                            y + t - ((sizeWindow - 1) / 2) >= 0 and
-                            x + u + ((sizeWindow - 1) / 2) < image.width() and
-                            y + t + ((sizeWindow - 1) / 2) < image.height()):
-                        tmp = tab_patch[x][y].compare_grid(tab_patch[u][t])
-                        if len(closest_patch) < NB_CLOSEST:
-                            closest_patch[len(closest_patch)] = (u, t, tmp)
-                        else:
-                            if dist_max(closest_patch) > tmp :
-                                closest_patch[dist_max(closest_patch)] = (u, t, tmp)
-                    #end if
-                # end for
-            #end for
-
-            dist_total = 0
-            for i in closest_patch:
-                dist_total += i[2]
-            pixel = 0
-            for i in closest_patch:
-                pixel += image.getPixel((i[0], i[1])) * dist_total / i[2]
-            res_image.putpixel((x,y),pixel)
+    def get_index_of_maximal_distance(self):
+        index = 0
+        maximum = self.patchs_array[0][2]
+        for i in range(self.patchs_array.__sizeof__()):
+            if maximum < self.patchs_array[i][2]:
+                index = i
+            # end if
         # end for
-    # end for
+        return index
+    # end def
 
-    return res_image
+    def init_patchs_array(self, size):
+        for x in range(self.denoised_image.width()):
+            for y in range(self.denoised_image.height()):
+                self.patchs_array[x][y] = Patch((x, y), size, self.noised_image)
+            # end for
+        # end for
+    # end def
+
+    def prepare_image(self):
+        if (self.noised_image.mode == """RGBA""") or (self.noised_image.mode == """RGB"""):
+            # Convert to a greyscale system
+            self.noised_image = self.noised_image.convert("""L""")
+        # end if
+    # end def
+
+    def run(self, patch_size, window_size):
+        self.init_patchs_array(patch_size)
+        closest_patch = []
+
+        # Image
+        for x in range(self.denoised_image.width()):
+            for y in range(self.denoised_image.height()):
+
+                # Window
+                for u in range(window_size):
+                    for t in range(window_size):
+                        if (x + u - ((window_size - 1) / 2) >= 0 and
+                                y + t - ((window_size - 1) / 2) >= 0 and
+                                x + u + ((window_size - 1) / 2) < self.denoised_image.width() and
+                                y + t + ((window_size - 1) / 2) < self.denoised_image.height()):
+                            tmp = self.patchs_array[x][y].compare_grid(self.patchs_array[u][t])
+                            if self.closest_patchs_array_current_size < self.closest_patchs_array_maximum_size:
+                                self.closest_patchs_array[self.closest_patchs_array_current_size] = (u, t, tmp)
+                                self.closest_patchs_array_current_size += 1
+                            else:
+                                if self.get_index_of_maximal_distance() > tmp:
+                                    self.closest_patchs_array[self.get_index_of_maximal_distance()] = (u, t, tmp)
+                        # end if
+                    # end for
+                # end for
+
+                dist_total = 0
+                for i in closest_patch:
+                    dist_total += i[2]
+                pixel = 0
+                for i in closest_patch:
+                    pixel += self.noised_image.getPixel((i[0], i[1])) * dist_total / i[2]
+                self.denoised_image.putpixel((x, y), pixel)
+            # end for
+        # end for
+    # end def
+
+    def show(self, choice):
+        if choice == """input""":
+            self.noised_image.show()
+        elif choice == """output""":
+            self.denoised_image.show()
+        else:
+            print("""Invalid parameter "choice" for Denoiser.show(choice)""")
+    # end def
+
+# end class
 
 
 if __name__ == """__main__""":
-    image = run_denoiser("noising_inpuy.png", 1, 10)
-    image.show()
-    print("""""")
+    denoiser = Denoiser("""pictures/input.png""")
+    denoiser.run(1, 10)
+    denoiser.show("""input""")
+    denoiser.show("""output""")
 # end if
